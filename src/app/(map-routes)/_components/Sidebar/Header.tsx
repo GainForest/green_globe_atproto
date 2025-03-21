1ccf0e4ae5aa73c22f591efe173fb557c9e19b6e
Satyam <2021pcecysatyam050@poornima.org>
2025-03-21 09:28:47 +0530

    feat: disply link to my projects when connected

--- src/app/(map-routes)/_components/Sidebar/Header.tsx
+++ src/app/(map-routes)/_components/Sidebar/Header.tsx
@@ -3,11 +3,18 @@
 import React from "react";
 import Image from "next/image";
 import dynamic from "next/dynamic";
+import { useAppKitAccount, useAppKit } from "@reown/appkit/react";
+import { User2 } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 const WalletButton = dynamic(() => import("@/app/_components/WalletButton"), {
   ssr: false,
 });
 
 const Header = () => {
+  const { address } = useAppKitAccount();
+  const { open } = useAppKit();
   return (
     <div className="flex items-center justify-between">
       <div className="flex items-center gap-2">
@@ -20,7 +27,18 @@ const Header = () => {
         />
         <span className="font-bold text-lg">GainForest</span>
       </div>
-      <WalletButton />
+      {address ? (
+        <div className="flex items-center gap-2">
+          <Link href="/my-projects">
+            <Button variant={"ghost"}>My Projects</Button>
+          </Link>
+          <Button variant={"outline"} size={"icon"} onClick={() => open()}>
+            <User2 />
+          </Button>
+        </div>
+      ) : (
+        <WalletButton />
+      )}
     </div>
   );
 };
