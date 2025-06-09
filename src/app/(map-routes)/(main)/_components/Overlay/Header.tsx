b108e879fdafdf169d07aadd624c4761b7f72e6b
Satyam Mishra <satyam1308mishra@gmail.com>
2025-06-09 12:08:59 +0530

    feat: display auth stuff on map routes

--- src/app/(map-routes)/(main)/_components/Overlay/Header.tsx
+++ src/app/(map-routes)/(main)/_components/Overlay/Header.tsx
@@ -2,14 +2,19 @@
 
 import React from "react";
 import Image from "next/image";
-
-// const WalletButton = dynamic(() => import("@/app/_components/WalletButton"), {
-//   ssr: false,
-// });
+import { useUserContext } from "@/app/_contexts/User";
+import { useStackedDialog } from "@/components/ui/StackedDialog/context";
+import { User2 } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { Loader2 } from "lucide-react";
+import Link from "next/link";
+import { usePrivy } from "@privy-io/react-auth";
 
 const Header = ({ showBrandName = true }: { showBrandName?: boolean }) => {
   // const { address } = useAppKitAccount();
   // const { open } = useAppKit();
+  const { authenticated, ready } = usePrivy();
+  const { openDialog } = useStackedDialog();
   return (
     <div className="flex items-center justify-between">
       <div className="flex items-center gap-2">
@@ -24,18 +29,34 @@ const Header = ({ showBrandName = true }: { showBrandName?: boolean }) => {
       </div>
 
       {/* Commenting this code to temporarily remove it */}
-      {/* {address ? (
+      {!ready ? (
+        <Button variant={"ghost"} size={"icon"}>
+          <Loader2 className="animate-spin" />
+        </Button>
+      ) : authenticated ? (
         <div className="flex items-center gap-2">
-          <Link href="/my-projects">
-            <Button variant={"ghost"}>My Projects</Button>
+          <Link href="/projects">
+            <Button variant={"ghost"} size={"sm"}>
+              Projects
+            </Button>
           </Link>
-          <Button variant={"outline"} size={"icon"} onClick={() => open()}>
+          <Button
+            variant={"outline"}
+            size={"icon"}
+            onClick={() => openDialog("account")}
+          >
             <User2 />
           </Button>
         </div>
       ) : (
-        <WalletButton />
-      )} */}
+        <Button
+          variant={"outline"}
+          onClick={() => openDialog("onboarding")}
+          size={"sm"}
+        >
+          Sign in
+        </Button>
+      )}
     </div>
   );
 };
